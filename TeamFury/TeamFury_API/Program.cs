95889eca7f00a96cbdaa34c75bcb4bc04a81acf4
Patrik Skattberg <patrikskattberg@hotmail.com>
2023-10-09 14:56:24 +0200

    feat: Identity options feat: on omodel create Admin x2 and User x2 fix: Request added string:AdminName

--- TeamFury/TeamFury_API/Program.cs
+++ TeamFury/TeamFury_API/Program.cs
@@ -1,5 +1,8 @@
 using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using TeamFury_API.Data;
 
 namespace TeamFury_API
 {
@@ -22,8 +25,25 @@ namespace TeamFury_API
                         ValidateIssuerSigningKey = true
                     };
                 });
+            builder.Services.Configure<IdentityOptions>(options =>
+            {
+                //Password Settings
+                options.Password.RequireDigit = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 5;
+
+                //Lockout Settings
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(2);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+
+                //User Settings
+                options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@";
+                options.User.RequireUniqueEmail = true;
+			});
             builder.Services.AddAuthorization();
-            
+            builder.Services.AddDbContext<AppDbContext>(opt =>
+                opt.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
             var app = builder.Build();
 
             
