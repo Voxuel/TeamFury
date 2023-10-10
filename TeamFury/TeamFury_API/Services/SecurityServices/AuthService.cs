710bb450d1c72644400abc7d264918f79b691d43
Voxuel <Leo.Fridh@hotmail.com>
2023-10-10 19:57:19 +0200

    add security services

---
+++ TeamFury/TeamFury_API/Services/SecurityServices/AuthService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Models.DTOs;
+
+namespace TeamFury_API.Services.SecurityServices;
+
+public class AuthService : IAuthService
+{
+    private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly IConfiguration _config;
+
+    public AuthService()
+    {
+        
+    }
+    
+    
+    public Task<(int, string)> Registration()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<(int, string)> Login(LoginDTO login)
+    {
+        throw new NotImplementedException();
+    }
+}
