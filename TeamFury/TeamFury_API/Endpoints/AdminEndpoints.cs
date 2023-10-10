b1eb5b1633f1a6743c5d0bf91dad278038083da8
Voxuel <Leo.Fridh@hotmail.com>
2023-10-10 12:56:03 +0200

    add admin policy and refactor endpoints

---
+++ TeamFury/TeamFury_API/Endpoints/AdminEndpoints.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using TeamFury_API.Data;
+
+namespace TeamFury_API.Endpoints;
+
+public static class AdminEndpoints
+{
+    public static void AdminEndpointConfig(this WebApplication app)
+    {
+        app.MapGet("/api/admin/", GetAllAsync).RequireAuthorization("IsAdmin");
+    }
+
+    private static async Task<IResult> GetAllAsync(AppDbContext context)
+    {
+        return Results.Ok(await context.Admins.ToListAsync());
+    }
+}
---
+++ TeamFury/TeamFury_API/Endpoints/SecurityEndpoints.cs
@@ -0,0 +1,64 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using Models.DTOs;
+
+namespace TeamFury_API.Endpoints;
+
+public static class SecurityEndpoints
+{
+
+    public static void SecurityConfig(this WebApplication app)
+    {
+        app.MapPost("/api/token/", CreateToken);
+    }
+    
+    
+    private static async Task<IResult> CreateToken(IConfiguration config , LoginDTO user)
+    {
+        if (user.Username == "Admin2" && user.Password == "MTG15")
+        {
+            var issuer = config["Jwt:Issuer"];
+            var audience = config["Jwt:Audience"];
+            var securityKey =
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            // Now its ime to define the jwt token which will be responsible of creating our tokens
+            var jwtTokenHandler = new JwtSecurityTokenHandler();
+
+            // We get our secret from the appsettings.
+            var key = Encoding.ASCII.GetBytes(config["Jwt:Key"]);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim("Id", "1"),
+                    new Claim(JwtRegisteredClaimNames.Sub, user.Username),
+                    new Claim(JwtRegisteredClaimNames.Email, user.Username),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                }),
+                // the life span of the token needs to be shorter and utilise refresh token to keep the user signedin
+                Expires = DateTime.UtcNow.AddHours(6),
+                Audience = audience,
+                Issuer = issuer,
+                // here we are adding the encryption alogorithim information which will be used to decrypt our token
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha512Signature)
+            };
+
+            var token = jwtTokenHandler.CreateToken(tokenDescriptor);
+
+            var jwtToken = jwtTokenHandler.WriteToken(token);
+
+            return Results.Ok(jwtToken);
+        }
+        else
+        {
+            return Results.Unauthorized();
+        }
+    }
+}
