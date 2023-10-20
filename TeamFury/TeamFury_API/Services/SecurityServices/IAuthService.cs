ad1ad36a82d9886c5f53e9e097e0e608dadd7941
Voxuel <Leo.Fridh@hotmail.com>
2023-10-20 14:33:24 +0200

    create service and endpoint for user password reset

--- TeamFury/TeamFury_API/Services/SecurityServices/IAuthService.cs
+++ TeamFury/TeamFury_API/Services/SecurityServices/IAuthService.cs
@@ -1,8 +1,10 @@
 ﻿using Models.DTOs;
+using Models.Models;
 
 namespace TeamFury_API.Services.SecurityServices;
 
 public interface IAuthService
 {
     Task<(int, string)> Login(LoginDTO login);
+    Task<StatusResult> UpdatePassword(string password, string id);
 }
