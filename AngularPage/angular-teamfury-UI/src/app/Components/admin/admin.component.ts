6d296b6ef10b79c0232f778c467030453eb68033
Voxuel <Leo.Fridh@hotmail.com>
2023-10-23 12:57:01 +0200

    start working on get leavedays

--- AngularPage/angular-teamfury-UI/src/app/Components/admin/admin.component.ts
+++ AngularPage/angular-teamfury-UI/src/app/Components/admin/admin.component.ts
@@ -4,6 +4,7 @@ import { AdminService } from 'src/app/Services/admin.service';
 import { AuthService } from 'src/app/Services/auth.service';
 import { UserService } from 'src/app/Services/user.service';
 import { Employee } from 'src/app/models/employee';
+import { LeaveDaysTotal } from 'src/app/models/leaveDaysTotal';
 import { UserViewModel } from 'src/app/models/user.view.model';
 
 @Component({
@@ -13,7 +14,12 @@ import { UserViewModel } from 'src/app/models/user.view.model';
 })
 export class AdminComponent {
 
+leavedays:LeaveDaysTotal[] = []
 
+  constructor(private adminService:AdminService){}
 
-  constructor(){}
+  getTotalLeavedays(){
+    this.adminService.getTotalUsedLeavedays().subscribe(response => {this.leavedays = response})
+  }
+
 }
