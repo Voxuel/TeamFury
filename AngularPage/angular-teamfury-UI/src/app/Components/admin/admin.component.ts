ae2fb138d10243d46ac785df7faafd6a92164f63
Voxuel <Leo.Fridh@hotmail.com>
2023-10-24 15:46:18 +0200

    add get all pending requests

--- AngularPage/angular-teamfury-UI/src/app/Components/admin/admin.component.ts
+++ AngularPage/angular-teamfury-UI/src/app/Components/admin/admin.component.ts
@@ -24,12 +24,12 @@ export class AdminComponent {
 
 leavedays:LeaveDaysTotal[] = []
 allRequests:RequestViewModel[] = []
+allPending:RequestViewModel[] = []
 form:FormGroup
 leaveType:RequestTypeCreate ={
   name:'',
   maxDays:''
 }
-errors:any[] = []
 
 constructor(private adminService:AdminService, private builder:FormBuilder, private _snackBar:MatSnackBar){
   this.form = builder.group({
@@ -48,7 +48,15 @@ constructor(private adminService:AdminService, private builder:FormBuilder, priv
     this.adminService.getTotalUsedLeavedays().subscribe(response => {this.leavedays = response})
   }
   getRequests(){
-    this.adminService.getAllRequests().subscribe(response => {this.allRequests = response})
+    this.adminService.getAllRequests().subscribe(response => {
+      response.forEach(element => {
+        if(element.statusRequest == '0'){
+          this.allPending.push(element)
+        }
+      },
+      this.allRequests = response
+      );
+    })
   }
 
 
@@ -68,5 +76,4 @@ constructor(private adminService:AdminService, private builder:FormBuilder, priv
     this.adminService.createRequestType(this.leaveType).subscribe()
     this._snackBar.open('Created')
   }
-
 }
