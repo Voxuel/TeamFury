82077218f206f6675c4cf49232e36fd4bcf93a99
Voxuel <Leo.Fridh@hotmail.com>
2023-10-27 17:19:57 +0200

    add jwt keyvaults

--- TeamFury/TeamFury_API/Program.cs
+++ TeamFury/TeamFury_API/Program.cs
@@ -118,6 +118,9 @@ namespace TeamFury_API
 
             #region Authentication/Authorization options.
 
+            KeyVaultSecret kvAd = client.GetSecret("IssuerAdu");
+            KeyVaultSecret kvJwt = client.GetSecret("AuthKey");
+            
             builder.Services.AddAuthentication(o =>
             {
                 o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -127,10 +130,10 @@ namespace TeamFury_API
             {
                 o.TokenValidationParameters = new TokenValidationParameters
                 {
-                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
-                    ValidAudience = builder.Configuration["Jwt:Audience"],
+                    ValidIssuer = kvAd.Value,
+                    ValidAudience = kvAd.Value,
                     IssuerSigningKey = new SymmetricSecurityKey
-                        (Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+                        (Encoding.UTF8.GetBytes(kvJwt.Value)),
                     ValidateIssuer = true,
                     ValidateAudience = true,
                     ValidateLifetime = false,
